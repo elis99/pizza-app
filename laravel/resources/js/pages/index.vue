4d9c187707090773cdc758e499a5ced7844e640a
Elis <elis.nor99@gmail.com>
2020-06-26 20:21:01 +0500

    babel fix

--- laravel/resources/js/pages/index.vue
+++ laravel/resources/js/pages/index.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-import Home from '../components/Home/Home'
+import Home from '@/components/Home/Home'
 
 export default {
     components: {
--- laravel/resources/js/pages/login.vue
+++ laravel/resources/js/pages/login.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-import Login from '../components/Login/Login'
+import Login from '@/components/Login/Login'
 
 export default {
     components: {
