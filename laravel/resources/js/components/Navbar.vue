f01f04554d33097910b7401f5b88cab82f83f471
Elis <elis.nor99@gmail.com>
2020-06-26 07:35:45 +0500

    vue && navbar

---
+++ laravel/resources/js/components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav>
+    <div class="nav-wrapper orange darken-3">
+      <a href="#" class="brand-logo center">
+        <img
+            alt="logo"
+        >
+      </a>
+      <ul id="nav-mobile" class="left hide-on-med-and-down">
+        <li><a href="sass.html">Sass</a></li>
+        <li><a href="badges.html">Components</a></li>
+        <li><a href="collapsible.html">JavaScript</a></li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
